98dc768bf3dcb70011633e7a516fcca207b5c3f7
Owner <vikaskumar.sfdx@gmail.com>
2026-03-21 23:43:08 +0530

    getRelatedListCount update

--- force-app/main/default/lwc/wireGetRelatedListCount/wireGetRelatedListCount.js
+++ force-app/main/default/lwc/wireGetRelatedListCount/wireGetRelatedListCount.js
@@ -1,12 +1,13 @@
 import { LightningElement, wire, api } from "lwc";
 import { getRelatedListCount } from "lightning/uiRelatedListApi";
 export default class WireGetRelatedListCount extends LightningElement {
+    //getRelatedListCount - Place it in record page of Account for example to get related Opportunities present on the Account
   error;
   responseData;
   @api recordId
   @wire(getRelatedListCount, {
     parentRecordId: '$recordId',
-    relatedListId: "Contacts",
+    relatedListId: "Opportunities",
   })
   listInfo({ error, data }) {
     if (data) {
