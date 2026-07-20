7a760a2a1d228dec96e5ed388bd41f4675328cb0
Owner <vikaskumar.sfdx@gmail.com>
2026-07-20 12:17:17 +0530

    Wire Methods with params

---
+++ force-app/main/default/lwc/productList/productList.js
@@ -0,0 +1,33 @@
+import { LightningElement, wire } from 'lwc';
+import getProducts from '@salesforce/apex/ProductController.getProducts';
+import getProductByCode from '@salesforce/apex/ProductController.getProductByCode';
+export default class ProductList extends LightningElement {
+    products;
+    error;
+    filteredProducts;
+
+    // Wire with property
+    @wire(getProducts)
+    wiredProducts({ error, data }) {
+        if (data) {
+            this.products = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.products = undefined;
+        }
+    }
+
+    // Wire with property
+    @wire(getProductByCode, {productCode: 'GC1060'})
+    wiredProductCode({ error, data }) {
+        if (data) {
+            this.filteredProducts = data;
+            console.log('this.filteredProducts', this.filteredProducts);
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.filteredProducts = undefined;
+        }
+    }
+}
